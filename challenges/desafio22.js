010f768b99697796e95ee58a249abeb695d0a982
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-12-19 18:55:18 -0300

    REQ 22 - Adicione a quantidade de vendas dos sanduíches por dia da semana

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -2,9 +2,21 @@
 // "vendasPorDia": [0, 0, 0, 0, 0, 0, 0]
 
 // O primeiro item desse array representa as vendas no domingo, o segundo item representa as vendas na segunda-feira, e assim sucessivamente até chegar ao último item, que representa as vendas no sábado.
+db.produtos.updateMany({},
+  { $push: { vendasPorDia: { $each:
+    [0, 0, 0, 0, 0, 0, 0] } } });
 
 // 2- Crie uma query que incremente as vendas de Big Mac às quartas-feiras em 60.
+db.produtos.updateMany(
+  { nome: "Big Mac" },
+  { $set: { "vendasPorDia.3": 60 } },
+);
 
 // 3- Crie uma query que incremente as vendas de todos os sanduíches de carne do tipo bovino aos sábados em 120.
+db.produtos.updateOne(
+  { tags: "bovino" },
+  { $inc: { "vendasPorDia.6": 120 } },
+);
 
 // 4- Crie uma query que retorne o nome e vendasPorDia de todos os documentos.
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 }).pretty();
