557062498d156d226a4acfb2ea6c1321bf4803e7
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-12-22 20:05:24 -0300

    REQ 24 - Ordene em todos os documentos os valores do array valoresNutricionais pelo campo percentual de forma decrescente

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,3 +1,10 @@
 // Crie uma query que faça a adição do valor muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40.
+db.produtos.updateMany(
+  { $and: [{ valoresNutricionais:
+    { $elemMatch: { tipo: "sódio" } } },
+    { valoresNutricionais: { $elemMatch: { percentual: { $gte: 40 } } } }] },
+    { $push: { tags: "muito sódio" } },
+);
 
 // Crie uma query que retorne o nome e tags de todos os documentos.
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 }).pretty();
--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,3 +1,4 @@
 // Crie uma query que faça a adição do valor contém sódio ao final do array tags nos produtos em que o percentual de sódio seja maior do que 20 e menor do que 40.
 
 // Crie uma query que retorne o nome e tags de todos os documentos.
+db.produtos.find({}, { _id: 0, nome: 1, tags: 1 }).pretty();
