a7ff8c6753c0cc4485aa03ab00303da3ae370adc
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-12-22 22:01:45 -0300

    REQ 30 - Remova o campo curtidas do item Big Mac

--- challenges/desafio31.js
+++ challenges/desafio31.js
@@ -1,1 +1,2 @@
 // Retorne o nome dos sanduíches em que o número de curtidas é maior que o número de sanduíches vendidos
+db.produtos.find({}, { _id: 0, nome: 1, descricaoSite: 1 }).pretty();
--- challenges/desafio32.js
+++ challenges/desafio32.js
@@ -1,1 +1,2 @@
 // Retorne o nome e a quantidade de vendas (vendidos) dos sanduíches em que o número de vendas é múltiplo de 5
+db.produtos.find({}, { _id: 0, nome: 1, descricaoSite: 1 }).pretty();
