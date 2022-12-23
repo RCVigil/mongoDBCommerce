d2e59ea92457d5759ca26e97189bde9487e136e4
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-12-22 21:34:02 -0300

    REQ 27 - Conte quantos produtos contém Mc no nome, sem considerar letras maiúsculas ou minúsculas

---
+++ challenges/desafio29.js
@@ -0,0 +1,3 @@
+// 1 - Crie uma query que faça a renomeação do campo descricao para descricaoSite em todos os documentos.
+
+// 2 - Crie uma query que retorne o nome e descricaoSite de todos os documentos.
---
+++ challenges/desafio30.js
@@ -0,0 +1,3 @@
+// 1 - Crie uma query que faça a remoção do campo curtidas do item Big Mac.
+
+// 2 - Crie uma query que retorne o nome para todos os documentos e curtidas (exceto para Big Mac).
